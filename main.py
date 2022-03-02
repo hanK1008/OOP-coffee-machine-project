9dd215fe2cefea424a3d074cef24f793e2b2029a
Mohammad Khan <mohdahmedk@gmail.com>
2022-03-02 21:53:23 +0530

    Testing how to commit without making repo on github

---
+++ main.py
@@ -0,0 +1,8 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+"""
+Will start tomorrow
+Testing how to Push with out making repo on github 
+"""
